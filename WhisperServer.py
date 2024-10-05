63c4451c7ebb4526bbc4a2e63a72ca7c451a04af
W4FI <patrickwfink+w4pfi@gmail.com>
2024-10-05 09:57:18 -0400

    Update WhisperServer.py

--- WhisperServer.py
+++ WhisperServer.py
@@ -39,8 +39,12 @@ def transcribe_and_diarize(file_path: str, diarization: bool = False) -> str:
 
         # Load pyannote speaker diarization pipeline with the token
         # Model source : https://huggingface.co/pyannote/speaker-diarization
+        Pipeline.from_pretrained(
+            "pyannote/speaker-segmentation", 
+            use_auth_token=huggingface_token
+        )
         diarization_pipeline = Pipeline.from_pretrained(
-            "pyannote/speaker-diarization@2.1", 
+            "pyannote/speaker-diarization", 
             use_auth_token=huggingface_token
         )
 
