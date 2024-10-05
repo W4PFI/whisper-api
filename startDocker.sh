fec06b88df4f0e5f6e490d3f5930a73147dd8786
W4FI <patrickwfink+w4pfi@gmail.com>
2024-10-05 09:12:40 -0400

    Update startDocker.sh

--- startDocker.sh
+++ startDocker.sh
@@ -4,4 +4,4 @@
 docker build -t whisper-api .
 
 # Run the Docker container
-docker run -d --name whisper-api -p 8001:8000 whisper-api
+docker run -d --name whisper-api -p 8001:8000 -e HUGGINGFACE_TOKEN=$HUGGINGFACE_TOKEN whisper-api
